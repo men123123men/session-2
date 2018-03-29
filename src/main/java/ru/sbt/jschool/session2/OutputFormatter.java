d5b754050b7b635bed9e9edf55aba7d30f55afdb
aslanbek <men123123men@yandex.ru>
2018-03-29 04:45:32 +0300

    Исправил замечания

--- src/main/java/ru/sbt/jschool/session2/OutputFormatter.java
+++ src/main/java/ru/sbt/jschool/session2/OutputFormatter.java
@@ -29,16 +29,33 @@ import java.util.Scanner;
 import java.util.function.Function;
 
 /**
+ * 2. Модифицировать программу для возможности добавления новых выводимых типов данных
+ Выделить интерфейс, который отвечает за конкретный тип данных.
+ Какие у него должны быть методы?
+
+ * Добавить поддержку типа данных TIMESTAMP. Вывод даты в формате dd.MM.yyyy HH:mm.SS.sss
+ * Добавить поддержку типа данных CUT_STR. Вывод строки фиксированной длины. Для более длиных строк в конце добавляется ...
+ Пример. Максимальная длина - 15
+ Hello, World!    - строка "Hello, World!" влезает
+ Hello, beatif... - строка "Hello, beatifull World!" не влезает
+ 1234556789012345 - строка из ровано 15 символов влезает
+ *
+ *
+ *
  */
 public class OutputFormatter {
+
 	{Locale.setDefault(Locale.FRANCE);}
+
     private PrintStream out;
+    private DecimalFormat forDouble = new DecimalFormat("###,##0.00");
+    private DecimalFormat forInteger = new DecimalFormat("###,##0");
+
     public OutputFormatter(PrintStream out) {
         this.out = out;
     }
 
     public void output(String[] names, Object[][] data) {
-        //TODO: implement me.
 
         int height = data.length; //  first index   высота   по оси Y
         int width = names.length; // second index   ширина   по оси Х
@@ -59,7 +76,7 @@ public class OutputFormatter {
             String currentStr;
 
             for (int i = 0; i < width; i++) {      // итерируемся по столбцам (Object[j][i])
-                current_ObjectToString_Function = get_ObjectToString_Function(classes[i]);
+                current_ObjectToString_Function = getObjectToStringFunction(classes[i]);
                 for (int j = 0; j < height; j++) {  // итерируемся по значениям в столбце
                     currentStr = current_ObjectToString_Function.apply(data[j][i]);
                     if (currentStr.length() > elementLettersCount[i])
@@ -69,6 +86,7 @@ public class OutputFormatter {
             }
             currentLineFormater = makeLineFormater(elementLettersCount, classes);
         }
+
         String linesSeparator = makeLineSeparator(elementLettersCount, "-", "+");
         String header = makeHeader(names, elementLettersCount);
 
@@ -91,27 +109,16 @@ public class OutputFormatter {
         return result.toString();
     }
 
-    private DecimalFormat forDouble = new DecimalFormat("###,##0.00");
-    private DecimalFormat forInteger = new DecimalFormat("###,##0");
-    		
-    		;
-    private Function<Object,String> get_ObjectToString_Function(Class clazz){
+    private Function<Object,String> getObjectToStringFunction(Class clazz){
         switch (clazz.getSimpleName()){
             case "Integer": return o-> o==null ? "-" : forInteger.format(o);
-//            	String.format("%,d",o).replace(',',' ');
-            case "Double":  return o-> o==null ? "-" :forDouble.format(o);
-//            	String.format("%,.2f",Double.valueOf(o.toString())).replace(',',' ').replace('.',',');
-            case "String":  return o-> o==null ? "-" :String.format("%s",o);
-            case "Date":    return o-> o==null ? "-" :String.format("%td.%<tm.00%<ty",o);
+            case "Double":  return o-> o==null ? "-" : forDouble.format(o);
+            case "String":  return o-> o==null ? "-" : String.format("%s",o);
+            case "Date":    return o-> o==null ? "-" : String.format("%td.%<tm.00%<ty",o);
             default: return Object::toString;
         }
     }
-    /**
-     * @param lengths     массив с требуемыми ширинами колонок
-     * @param lineElement элементы из которых будет конструироваться линия
-     * @param crosshair   элемент на перекрестии
-     * @return String of lineElement divided by crosshair.
-     */
+
     private static String makeLineSeparator(int[] lengths,String lineElement, String crosshair){
         StringBuilder resultLine = new StringBuilder(crosshair);
         for(int wigthOfCurrentColumn:lengths){
@@ -129,49 +136,5 @@ public class OutputFormatter {
         for (int i=0;i<classes.length;i++)
             result.append("%"+(classes[i]==String.class?"-":"")+lengths[i]+"s|");
         return result.append("%n").toString();
-    }
-
-
-
-    public static void main(String[] args) throws Exception {
-//        OutputFormatter formatter = new OutputFormatter(System.out);
-//        for(int i = 0;i<3;i++)
-//            test(formatter,i);
-    }
-    private static Object format(String str, String type) throws ParseException {
-        if ("".equals(str))
-            return null;
-
-        switch (type) {
-            case "string":
-                return str;
-            case "number":
-                return Integer.valueOf(str);
-            case "date":
-                return new SimpleDateFormat("dd.MM.yyyy").parse(str);
-            case "money":
-                return Double.valueOf(str);
-        }
-
-        throw new RuntimeException("Unknown data type: " + type);
-    }
-    private static void test(OutputFormatter exeplair, int fileNumber) throws Exception {
-
-        String pathStr = "/Users/admin/eclipse-workspace/session-2/src/test/resources/"+fileNumber+"/input.csv";
-        System.out.println(pathStr);
-
-        Scanner sc = new Scanner(new FileInputStream(pathStr));
-        int size = Integer.valueOf(sc.nextLine());      // число строк
-        String[] types = sc.nextLine().split(",");      // типы столбцов (number,string,money,date)
-        String[] names = sc.nextLine().split(",");      // шапка таблиц(#,Дата,Число,ФИО,Зарплата )  приходит первым аргументом
-        Object[][] data = new Object[size][];           // колличество строк в 2D массиве берется из файла
-        for (int i=0; i<size; i++) {
-            String[] strLine = sc.nextLine().split(",", -1);
-            Object[] line = new Object[strLine.length];
-            for (int j = 0; j < strLine.length; j++)
-                line[j] = format(strLine[j], types[j]);
-            data[i] = line;
-        }
-        exeplair.output(names, data);
     }
 }
